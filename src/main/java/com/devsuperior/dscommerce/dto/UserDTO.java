914c4c53142abe0d7ee1d6c94ca1be51730164de
Maxwell-Luan <hydewinter04@gmail.com>
2026-07-09 17:05:01 -0300

    GET user logged

---
+++ src/main/java/com/devsuperior/dscommerce/dto/UserDTO.java
@@ -0,0 +1,56 @@
+package com.devsuperior.dscommerce.dto;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+
+import com.devsuperior.dscommerce.entities.User;
+
+public class UserDTO {
+
+	private Long id;
+	private String name;
+	private String email;
+	private String phone;
+	private LocalDate birthDate;
+	private List<String> roles = new ArrayList<>();
+	
+	public UserDTO(User entity) {
+		this.id = entity.getId();
+		this.name = entity.getName();
+		this.email = entity.getEmail();
+		this.phone = entity.getPhone();
+		this.birthDate = entity.getBirthDate();
+		entity.getRoles()
+				.stream()
+				.map(role -> role.getAuthority())
+				.forEach(roleName -> roles.add(roleName));
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public List<String> getRoles() {
+		return roles;
+	}
+	
+}
